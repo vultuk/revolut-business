8acc3afcdff9504f213b38e3f0cd3504fb0f6810
Simon Skinner <simon@sisk-ltd.co.uk>
2018-04-30 18:09:20 +0100

    chore(Build): New Build created

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-<a name="1.0.0"></a>
-# [1.0.0](https://github.com/vultuk/revolut-business/compare/v1.0.1...v1.0.0) (2018-04-30)
+<a name="1.0.1"></a>
+# [1.0.1](https://github.com/vultuk/revolut-business/compare/v1.0.1...v1.0.0) (2018-04-30)
 
 
 ### Bug Fixes
--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,31 +1,31 @@
 import * as request from 'request-promise-native';
-import {Config} from '../types';
+import { Config } from '../types';
 
 export class Action {
-    protected config: Config = undefined;
-    private readonly productionUrl: string = 'https://b2b.revolut.com/api/1.0/';
-    private readonly sandboxUrl: string = 'https://sandbox-b2b.revolut.com/api/1.0/';
+  protected config: Config = undefined;
+  private readonly productionUrl: string = 'https://b2b.revolut.com/api/1.0/';
+  private readonly sandboxUrl: string = 'https://sandbox-b2b.revolut.com/api/1.0/';
 
-    constructor(config?: Config) {
-        if (!config && !config.apiKey && !process.env.REVOLUT_API_KEY) {
-            throw new Error('No Revolut API key was provided.');
-        }
-
-        if (!config) {
-            this.config = {
-                apiKey: process.env.REVOLUT_API_KEY,
-                sandbox: process.env.REVOLUT_SANDBOX.toLowerCase() === 'true' || false,
-            } as Config;
-        } else {
-            this.config = config;
-        }
+  constructor(config?: Config) {
+    if (!config && !config.apiKey && !process.env.REVOLUT_API_KEY) {
+      throw new Error('No Revolut API key was provided.');
     }
 
-    protected async requestGet(endpoint: string): Promise<any> {
-        return request.get(
-            `${
-                this.config.sandbox ? this.sandboxUrl : this.productionUrl
-                }${endpoint}`,
-        );
+    if (!config) {
+      this.config = {
+        apiKey: process.env.REVOLUT_API_KEY,
+        sandbox: process.env.REVOLUT_SANDBOX.toLowerCase() === 'true' || false,
+      } as Config;
+    } else {
+      this.config = config;
     }
+  }
+
+  protected async requestGet(endpoint: string): Promise<any> {
+    return request.get(
+      `${
+        this.config.sandbox ? this.sandboxUrl : this.productionUrl
+      }${endpoint}`,
+    );
+  }
 }
